c16cdf9b22f7c8584bc93525c64874eec4b72898
songbiwen <1099790014@qq.com>
2020-10-12 16:23:31 +0800

    + init

---
+++ MyRxSwiftDemo/MyRxSwiftDemo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  MyRxSwiftDemo
+//
+//  Created by LongTu Qi on 2020/10/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
