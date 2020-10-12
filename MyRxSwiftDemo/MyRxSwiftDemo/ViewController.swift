6632353a91d5997e4ba8b698070bf6dd671247e3
songbiwen <1099790014@qq.com>
2020-10-12 17:15:27 +0800

    + 输入验证

--- MyRxSwiftDemo/MyRxSwiftDemo/ViewController.swift
+++ MyRxSwiftDemo/MyRxSwiftDemo/ViewController.swift
@@ -6,16 +6,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+private let minimalUsernameLength = 5
+private let minimalPasswordLength = 5
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!////用户名
+    @IBOutlet weak var usernameErrorLabel: UILabel!
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!//密码
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        view.backgroundColor = UIColor.red
+        usernameErrorLabel.text = "Username has to be at least \(minimalUsernameLength) characters"
+        passwordErrorLabel.text = "Password has to be at least \(minimalPasswordLength) characters"
+        
+        let usernameValid = usernameTextField.rx.text.orEmpty
+            .map{$0.count >= minimalUsernameLength}
+            .share(replay: 1, scope: .whileConnected)
+        let passwordValid = passwordTextField.rx.text.orEmpty
+            .map{$0.count >= minimalPasswordLength}
+            .share(replay: 1, scope: .whileConnected)
+        let everythingValid = Observable.combineLatest(usernameValid, passwordValid){$0 && $1}
+            .share(replay: 1, scope: .whileConnected)
+        
+        usernameValid.bind(to: passwordTextField.rx.isEnabled).disposed(by: disposeBag)
+        usernameValid.bind(to: usernameErrorLabel.rx.isHidden).disposed(by: disposeBag)
+        passwordValid.bind(to: passwordErrorLabel.rx.isHidden).disposed(by: disposeBag)
+        everythingValid.bind(to: actionButton.rx.isEnabled).disposed(by: disposeBag)
+        
+        actionButton.rx.tap
+            .subscribe(onNext:{[weak self] in self?.showAlert()})
+            .disposed(by: disposeBag)
+        
     }
 
 
+    func showAlert() {
+        
+        view.endEditing(true)
+        let alertVC = UIAlertController.init(title: "RxExample", message: "This is wonderful", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Done", style: .default, handler: { (doneAction) in
+            
+        }))
+        present(alertVC, animated: true) {
+            
+        }
+        
+    }
 }
 
---
+++ MyRxSwiftDemo/Pods/Target Support Files/Pods-MyRxSwiftDemo/Pods-MyRxSwiftDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyRxSwiftDemo : NSObject
+@end
+@implementation PodsDummy_Pods_MyRxSwiftDemo
+@end
